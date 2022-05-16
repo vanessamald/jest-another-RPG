98e93797c2f2330effb311c9a4eda086a024dd98
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-05-15 22:39:15 -0500

    refactor code to handle random potion creation

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -6,7 +6,7 @@ test('gets random number between 1 and 10', () => {
 });
 
 function randomNumber() {
-    return Math.floor(MAth.random() * 9 +1);
+    return Math.floor(Math.random() * 9 +1);
 }
 
 module.exports = randomNumber;
